3336ec42c81202444fc612b2afe7b668f62afba1
hisonlin <hisonlinca@gmail.com>
2023-02-20 12:40:33 -0800

    update midterm

--- midterm_part2/script.js
+++ midterm_part2/script.js
@@ -1,10 +1,26 @@
 const APIKEY = "9cf5e5c91b5056eadafdd94f4d14446f";
 let imagesData;
+let genreLookup = {};
 
-
-const fetchMarvel = async () => {
+//Fetch the genre list
+const fetchGenres = async () => {
     try {
-        const response = await axios.get(`https://api.themoviedb.org/3/search/movie?api_key=${APIKEY}&query=marvel`);
+      const response = await axios.get(`https://api.themoviedb.org/3/genre/movie/list?api_key=${APIKEY}`);
+      const genres = response.data.genres;
+      genreLookup = genres.reduce((acc, genre) => {
+        acc[genre.id] = genre.name;
+        return acc;
+      }, {});
+    } catch(error) {
+      console.log(error);
+      alert("There was an error", error);
+    }
+  }
+
+//Fetch movie info for home page
+const fetchTrending = async () => {
+    try {
+        const response = await axios.get(`https://api.themoviedb.org/3/trending/movie/week?api_key=${APIKEY}`);
         return response.data;
     } catch(error) {
         console.log(error);
@@ -12,7 +28,8 @@ const fetchMarvel = async () => {
     }
 }
 
-const fetchImageByKeyword = async () => {
+//Fetch movie info for searching
+const fetchByKeyword = async () => {
 
     const keyword = document.getElementById('searched-keyword').value;
 
@@ -28,65 +45,74 @@ const fetchImageByKeyword = async () => {
     }
 }
 
-const generateUI = (arrayOfImages) => {
-    let photoSection = document.getElementById('photo-section');
-    photoSection.innerHTML = "";
+//Get data we want by fetching genres and trending movie for home page
+async function getMovie() {
+    await fetchGenres(); // Fetch genre data first
+    const { results } = await fetchTrending();
+    imagesData = results.map((object) => {
+        return {
+            url: object.poster_path,
+            title: object.title,
+            released_year: object.release_date,
+            description: object.overview || "Random Description",
+            genre: object.genre_ids
 
-    arrayOfImages.forEach((object) => {
-        let imageContainer = document.createElement('div');
+        }
+    });
 
-        imageContainer.innerHTML = `
-        <img src=${modifyPosterPath(object.url)}>
-        <p><strong>Title:</strong> ${object.title}</p>
-        <time><strong>Released Year:</strong> ${ modifyDate(object.released_year) }</time>
-        <p><strong>Description:</strong> ${object.description}</p>
-        <p><strong>Genre:</strong> ${object.genre}</p>
-        `
-        photoSection.appendChild(imageContainer);
-    })
-
+    generateUI(imagesData);
 }
 
+//Get data we want by fetching genres and related by searching keyword
+async function getMovieByKeyword() {
+    const { results } = await fetchByKeyword();
+
+    imagesData = results.map((object) => {
+        return {
+            url: object.poster_path,
+            title: object.title,
+            released_year: object.release_date,
+            description: object.overview || "Random Description",
+            genre: object.genre_ids
+
+        }
+    });
+
+    generateUI(imagesData);
+}
+
+//Modify released_date to show year only
 function modifyDate(release_date) {
     const year = (new Date(release_date)).getFullYear();
     return year;
 }
+
+//Modify the img path
 function modifyPosterPath(poster_path){
     const imgPath = "https://image.tmdb.org/t/p/original/"
     poster_path = imgPath + poster_path;
     return poster_path;
 }
 
-async function getImageDataByKeyword() {
-    const { results } = await fetchImageByKeyword();
+//Generate UI
+const generateUI = (arrayOfImages) => {
+    let photoSection = document.getElementById('photo-section');
+    photoSection.innerHTML = "";
+  
+    arrayOfImages.forEach((object) => {
+      let imageContainer = document.createElement('div');
+  
+      const genreNames = object.genre.map(id => genreLookup[id]).join(', ');
+  
+      imageContainer.innerHTML = `
+        <img src=${modifyPosterPath(object.url)}>
+        <p><strong>Title:</strong> ${object.title}</p>
+        <time><strong>Released Year:</strong> ${ modifyDate(object.released_year) }</time>
+        <p><strong>Description:</strong> ${object.description}</p>
+        <p><strong>Genre:</strong> ${genreNames}</p>
+      `
+      photoSection.appendChild(imageContainer);
+    })
+  }
 
-    imagesData = results.map((object) => {
-        return {
-            url: object.poster_path,
-            title: object.title,
-            released_year: object.release_date,
-            description: object.overview || "Random Description",
-            genre: object.genre_ids
-
-        }
-    });
-
-    generateUI(imagesData);
-}
- 
-async function getData() {
-    const { results } = await fetchMarvel();
-    imagesData = results.map((object) => {
-        return {
-            url: object.poster_path,
-            title: object.title,
-            released_year: object.release_date,
-            description: object.overview || "Random Description",
-            genre: object.genre_ids
-
-        }
-    });
-
-    generateUI(imagesData);
-}
-getData();
+getMovie();
